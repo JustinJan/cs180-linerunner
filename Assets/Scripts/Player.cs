b9ce01e44f52b4fca383e82b194cd7405aaf1016
nchou001 <nchou001@ucr.edu>
2015-10-09 13:42:29 -0700

    Initial game commit
    Created initial scene + player gameobject

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D playerbody;
+
+	// Use this for initialization
+	void Start () {
+		playerbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerbody.velocity = new Vector2 (speed, playerbody.velocity.y);
+	}
+}
