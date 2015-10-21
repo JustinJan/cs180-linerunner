24f835580e54184cad56071482c9d402811c8b45
nchou001 <nchou001@ucr.edu>
2015-10-21 16:37:47 -0700

    Added crouching and stuff
    -Player no longer jumps multiple times in the air
    -Player can now crouch
    -Detached main camera from player
    -Main camera no longer follows player in y direction

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+	private Transform player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (player.position.x+3, 0, -1f);
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,23 +6,47 @@ public class Player : MonoBehaviour {
 	public float speed;
 	public float jumpspeed;
 	private Rigidbody2D playerbody;
+	//private float distToGround;
 
 	// Use this for initialization
 	void Start () {
 		playerbody = GetComponent<Rigidbody2D>();
+		//distToGround = GetComponent<Collider2D>().bounds.extents.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		playerbody.velocity = new Vector2 (speed, playerbody.velocity.y);
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			playerbody.velocity = new Vector2 (playerbody.velocity.x, jumpspeed);
+			if(isGrounded ()){
+			    playerbody.velocity = new Vector2 (playerbody.velocity.x, jumpspeed);
+			}
+			else{
+				Debug.DrawRay (transform.position, Vector3.down, Color.green);
+			}
 		}
+		if (Input.GetKeyDown (KeyCode.LeftControl)) {
+			//transform.position = new Vector3(transform.position.x, .1f, 0);
+			transform.localScale = new Vector3(transform.localScale.x, .5f, 0);
+		}
+		if (Input.GetKeyUp (KeyCode.LeftControl)) {
+			//transform.position = new Vector3(transform.position.x, .5f, 0);
+			transform.localScale = new Vector3(transform.localScale.x, 1f, 0);
+		}
+
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.tag == "Obstacle") {
 			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+
+	bool isGrounded(){
+		if (playerbody.velocity.y == 0) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
