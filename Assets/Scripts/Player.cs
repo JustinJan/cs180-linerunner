b98c97ef0f93c10b6455dae4f8d83ed613c207f6
Andrew Nava-Juarez <anava014@ucr.edu>
2015-10-21 15:16:23 -0700

    Spawn obstacles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,16 +6,24 @@ public class Player : MonoBehaviour {
 	public float speed;
 	public float jumpspeed;
 	private Rigidbody2D playerbody;
+	Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		playerbody = GetComponent<Rigidbody2D>();
+		anim = GetComponent <Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(playerbody.position.y <= 2)
+			anim.SetFloat("Speed", 0);
+		else {
+			anim.SetFloat("Speed", 1);
+		}
 		playerbody.velocity = new Vector2 (speed, playerbody.velocity.y);
 		if (Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetFloat("Speed", 1);
 			playerbody.velocity = new Vector2 (playerbody.velocity.x, jumpspeed);
 		}
 	}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+	using System.Collections;
+	
+	public class SpawnManager : MonoBehaviour {
+		
+		public int maxPlatforms = 20;
+		public GameObject platform;
+		public float horizontalMin = 7.5f;
+		public float horizontalMax = 14f;
+		public float verticalMin = -1;
+		public float verticalMax = 2f;
+		
+		
+		private Vector2 originPosition;
+		
+		
+		void Start () {
+			
+			originPosition = transform.position;
+			Spawn ();
+			
+		}
+		
+		void Spawn()
+		{
+			for (int i = 0; i < maxPlatforms; i++)
+			{
+				int isFloor = Random.Range(0,2);
+				int position;
+				if(isFloor == 1)
+					position = 0;
+				else
+					position = 3;
+				Vector2 randomPosition = originPosition + new Vector2 (Random.Range(horizontalMin, horizontalMax), position);
+				Instantiate(platform, randomPosition, Quaternion.identity);
+				originPosition.x = randomPosition.x;
+				//if (originPosition.y < 2) {
+				//    originPosition.y = 2;
+			    //}
+			    if (originPosition.y > 5) {
+				    originPosition.y = 5;
+			    }
+				
+			}  
+		}
+		
+	}
