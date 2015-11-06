6d4caa666ae5cee8787a77759deec7c18782b4e4
Andrew Nava-Juarez <anava014@ucr.edu>
2015-11-06 01:35:07 -0800

    Added animations and new level

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,19 +7,31 @@ public class Player : MonoBehaviour {
 	public float jumpspeed;
 	private Rigidbody2D playerbody;
 	private bool holdjump;
+	private Vector3 spawnPoint;
+	Animator anim;
 	//private float distToGround;
 
 	// Use this for initialization
 	void Start () {
+		spawnPoint = transform.position;
 		playerbody = GetComponent<Rigidbody2D>();
 		//distToGround = GetComponent<Collider2D>().bounds.extents.y;
 		holdjump = false;
+		anim = GetComponent <Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		playerbody.velocity = new Vector2 (speed, playerbody.velocity.y);
+
+		if(playerbody.position.y <= .03)
+			anim.SetFloat("Speed", 0);
+		else 
+			anim.SetFloat("Speed", 1);
+
+
 		if (Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetFloat("Speed", 1);
 			if(isGrounded ()){
 			    playerbody.velocity = new Vector2 (playerbody.velocity.x, jumpspeed);
 				holdjump = true;
@@ -32,6 +44,7 @@ public class Player : MonoBehaviour {
 			}
 		}
 		if (Input.GetKeyUp (KeyCode.Space)) {
+
 			playerbody.gravityScale = 18;
 			holdjump = false;
 		}
@@ -40,19 +53,22 @@ public class Player : MonoBehaviour {
 			//transform.position = new Vector3(transform.position.x, .1f, 0);
 			transform.localScale = new Vector3(transform.localScale.x, .5f, 0);
 			playerbody.gravityScale = 22;
+
 		}
 		if (Input.GetKeyUp (KeyCode.LeftControl)) {
 			//transform.position = new Vector3(transform.position.x, .5f, 0);
 			transform.localScale = new Vector3(transform.localScale.x, 1f, 0);
 			playerbody.gravityScale = 18;
+			anim.SetFloat("Speed", 0);
 		}
 
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
-		if (other.tag == "Obstacle") {
-			Application.LoadLevel (Application.loadedLevel);
-		}
+//		if (other.tag == "Obstacle") {
+//			Application.LoadLevel (Application.loadedLevel);
+//		}
+		transform.position = spawnPoint;
 	}
 
 	bool isGrounded(){
