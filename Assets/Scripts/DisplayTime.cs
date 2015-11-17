2d940765f4c366412d66e84fe439b845dba19006
bseto <brittany.seto@yahoo.com>
2015-11-17 12:18:53 -0800

    Added time to upper left corner

--- Assets/Scripts/DisplayTime.cs
+++ Assets/Scripts/DisplayTime.cs
@@ -24,19 +24,10 @@ public class DisplayTime : MonoBehaviour
 	}
 
 	void OnGUI(){
-
-//		if (GUI.Button(new Rect(100, 100, 40,40), "ShowTime")){
-//			if (timeVisible)
-//				timeVisible = false; 
-//			else 
-//				timeVisible = true; 
-//		}
-		//if(timeVisible)
 		GUI.color = Color.gray; 
 		GUI.skin.box.fontStyle = FontStyle.Bold;
 		GUI.skin.label.fontSize = 20;
 		string text = string.Format ("{0:00}:{1:00}", minutes, seconds);
-		//GUI.Label(new Rect(50, 20, 400, 50), minutes.ToString () + " minute, " + seconds.ToString () + " seconds");
 		GUI.Label(new Rect(50, 20, 400, 50), text);
 	}
 
