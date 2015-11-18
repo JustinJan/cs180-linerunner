b50bc6577b1ffb84f7e7868f55be162a46bb3225
bseto <brittany.seto@yahoo.com>
2015-11-17 19:28:30 -0800

    Changed captions in tutorial scene and added milliseconds for time

--- Assets/Scripts/DisplayTime.cs
+++ Assets/Scripts/DisplayTime.cs
@@ -7,28 +7,31 @@ public class DisplayTime : MonoBehaviour
 	public int playtime = 0; 
 	private int seconds = 0; 
 	private int minutes = 0; 
+	private int milliseconds = 0; 
 	private bool timeVisible = true; 
-
+	
 	void Start(){
 		StartCoroutine ("PlayTimer");
 	}
-
+	
+	//Minutes:Seconds:Milliseconds
 	private IEnumerator PlayTimer(){
 		while (true) {
-			yield return new WaitForSeconds (1); 
-			playtime += 1;
-			seconds = (playtime % 60); 
-			minutes = (playtime / 60) % 60; 
+			yield return new WaitForSeconds (0.01f); 
+			playtime += 10;
+			milliseconds = playtime % 1000; 
+			seconds = (playtime / 1000) % 60; 
+			minutes = (playtime / 60000) % 60; 
 		}
-
+		
 	}
-
+	
 	void OnGUI(){
 		GUI.color = Color.gray; 
 		GUI.skin.box.fontStyle = FontStyle.Bold;
 		GUI.skin.label.fontSize = 20;
-		string text = string.Format ("{0:00}:{1:00}", minutes, seconds);
+		string text = string.Format ("{0:00}:{1:00}:{2:00}", minutes, seconds, milliseconds);
 		GUI.Label(new Rect(50, 20, 400, 50), text);
 	}
-
+	
 }
