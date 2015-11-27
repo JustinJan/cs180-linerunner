f0958c3887484f1a2f6ce0fbd157beb009f237d6
bseto <brittany.seto@yahoo.com>
2015-11-27 00:44:20 -0800

    Display coins collected under time/ Added coins to Andrew's level

--- Assets/Scripts/coinScript.cs
+++ Assets/Scripts/coinScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class coinScript : MonoBehaviour {
 
+	public static int coins = 0; 
 	// Use this for initialization
 	void Start () {
 	
@@ -12,9 +13,18 @@ public class coinScript : MonoBehaviour {
 	void Update () {
 	
 	}
+
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.gameObject.CompareTag ("Player")){
 			Destroy(gameObject);
+			coins++; 
 		}
+	}
+
+	void OnGUI(){
+		GUI.color = Color.gray; 
+		GUI.skin.box.fontStyle = FontStyle.Bold;
+		GUI.skin.label.fontSize = 20;	
+		GUI.Label(new Rect(50, 50, 400, 50), "Coins: " + coins);
 	}
 }
