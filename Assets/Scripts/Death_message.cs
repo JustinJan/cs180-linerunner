0d0eba623459eae62e93ae97f9075efae989348c
David <dton001@ucr.edu>
2015-12-01 15:51:15 -0800

    Able to display coins when died and completed

--- Assets/Scripts/Death_message.cs
+++ Assets/Scripts/Death_message.cs
@@ -7,6 +7,7 @@ public class Death_message : MonoBehaviour {
 	{
 		string message = "Highest Score for this level: " + DisplayHighScore.return_highscore();
 		message = message + "\n Your current score: " + DisplayHighScore.format(DisplayTime.playtime);
+		message = message + "\n Your total amount of coins: " + coinScript.coins;
 
 		GUI.color = Color.black;
 		GUI.Label(new Rect(400, 120, 400, 500), message);
