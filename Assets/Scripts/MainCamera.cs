8e975144207b9679f9efecddaeabebaf147ffc98
nchou001 <nchou001@ucr.edu>
2015-12-04 01:18:43 -0800

    fixed shit

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+	private Transform player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (player.position.x+10, 3, -1f);
+	}
+}
---
+++ Assets/Scripts/coinScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coinScript : MonoBehaviour {
+
+	public static int coins = 0; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.gameObject.CompareTag ("Player")){
+			Destroy(gameObject);
+			coins++; 
+		}
+	}
+
+	void OnGUI(){
+		GUI.color = Color.gray; 
+		GUI.skin.box.fontStyle = FontStyle.Bold;
+		GUI.skin.label.fontSize = 20;	
+		GUI.Label(new Rect(50, 50, 400, 50), "Coins: " + coins);
+	}
+}
